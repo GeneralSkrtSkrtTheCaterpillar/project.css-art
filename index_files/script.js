e1d6739a097750a13ea002b36c69c35663a46286
makenzie compton <30756371+GeneralSkrtSkrtTheCaterpillar@users.noreply.github.com>
2017-08-11 22:21:22 -0500

    bodlight

---
+++ index_files/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+$("#sound").click(function(){
+$("#sound").append('<embed id="embed_player" src="audio/Rowlet.wav" autostart="true" hidden="true"></embed>');
+
+
+})
+
+})
