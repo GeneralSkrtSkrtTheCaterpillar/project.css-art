1d00a0b7c1e5766d7a4a06e25b18e7594bb04281
makenzie compton <30756371+GeneralSkrtSkrtTheCaterpillar@users.noreply.github.com>
2017-08-12 01:46:06 -0500

    leaf

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
 $("#sound").click(function(){
 $("#sound").append('<embed id="embed_player" src="audio/Rowlet.wav" autostart="true" hidden="true"></embed>');
 
-
 })
 
 })
