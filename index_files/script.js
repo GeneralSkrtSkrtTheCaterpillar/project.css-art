b260321499b1532320240f44dca57956d303e535
makenzie compton <30756371+GeneralSkrtSkrtTheCaterpillar@users.noreply.github.com>
2017-10-12 01:43:17 -0500

    NFT

--- index_files/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-$("#sound").click(function(){
-$("#sound").append('<embed id="embed_player" src="audio/Rowlet.wav" autostart="true" hidden="true"></embed>');
-
-
-})
-
-})
